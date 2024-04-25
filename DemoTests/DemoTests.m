35b77262a234fbf10136205351b358958145c696
Frederik Carlier <frederik.carlier@keysight.com>
2024-04-25 11:47:59 +0200

    Test encoding of keyEquivalentModifierMask for NSMenuItem and NSButtonCell

--- DemoTests/DemoTests.m
+++ DemoTests/DemoTests.m
@@ -21,7 +21,64 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
+// menu item encoding uses NSKeyEquivModMask, with no byte shift
+- (void)testMenuItemEncoding {
+    NSMenuItem* item = [[NSMenuItem alloc] init];
+    item.keyEquivalentModifierMask = NSShiftKeyMask;
+    NSNumber* encodedKeyMask = [self getEncodedKeyMask:item withKey:@"NSKeyEquivModMask"];
+    
+    XCTAssertNotNil(encodedKeyMask);
+    XCTAssertEqual([encodedKeyMask intValue], NSShiftKeyMask);
+}
+
+// button cell encoding uses NSButtonFlags2, with a 8-bit left shift
+- (void)testButtonCellEncoding {
+    int mask = NSShiftKeyMask;
+    
+    NSButtonCell* item = [[NSButtonCell alloc] init];
+    item.keyEquivalent = @"A";
+    item.keyEquivalentModifierMask = mask;
+    NSNumber* encodedKeyMask = [self getEncodedKeyMask:item withKey:@"NSButtonFlags2"];
+    
+    XCTAssertNotNil(encodedKeyMask);
+    NSLog(@"Got 0x%x; expecting 0x%x", [encodedKeyMask intValue], mask << 8);
+    XCTAssertEqual(([encodedKeyMask intValue] & NSEventModifierFlagDeviceIndependentFlagsMask << 8), mask << 8);
+}
+
+
+- (NSNumber*)getEncodedKeyMask:(id)item withKey:(NSString*) key {
+    NSKeyedArchiver* archiver =[[NSKeyedArchiver alloc] init];
+    [archiver encodeRootObject:item];
+    [archiver finishEncoding];
+    
+    NSData* data = [archiver encodedData];
+    NSError* error;
+    NSDictionary* archive = [NSPropertyListSerialization propertyListWithData:data options:NSPropertyListImmutable format:nil error:&error];
+    
+    NSArray* topLevelObjects = [archive objectForKey:@"$objects"];
+    
+    NSDictionary* dict;
+    
+    for (id element in topLevelObjects) {
+        if ([element isKindOfClass:[NSDictionary class]]) {
+            dict = (NSDictionary*)element;
+            
+            if ([[dict allKeys] containsObject:key]) {
+                break;
+            } else {
+                dict = nil;
+            }
+        }
+    }
+    
+    if (dict == nil) {
+        return nil;
+    }
+    
+    return [dict valueForKey:key];
+}
+
+- (void)testXibLoading {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     NSArray* topLevelObjects;
